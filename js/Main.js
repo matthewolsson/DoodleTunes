92c56228990916222b8cde107b2a98680c88d92e
matthewolsson <molsson2@gmail.com>
2015-02-21 04:33:34 -0500

    Cleaned up a little.
    Ran some tests with detecting red pixels. Gonna do some edge detection
    tests soon

--- js/Main.js
+++ js/Main.js
@@ -5,10 +5,10 @@ var c,ctx,img,playMusicBtn,findNotesBtn,musicPlaying,fps,count;
 window.onload = init;
 
 function init(){
-	c = document.getElementById("musicalPic");
-	ctx = c.getContext("2d");
+    c = document.getElementById("musicalPic");
+    ctx = c.getContext("2d");
 
-	$('#imageUploader').change(function () {
+    $('#imageUploader').change(function () {
         if (this.files && this.files[0]) {
             var reader = new FileReader();
             reader.onload = imageIsLoaded;
@@ -21,7 +21,7 @@ function init(){
     playMusicBtn=document.getElementById("playMusic");
     playMusicBtn.onclick = playMusic;
     findNotesBtn=document.getElementById("findNotes");
-    findNotesBtn.onclick = function(){searchForRectangle(); searchForNotes();};
+    findNotesBtn.onclick = searchForNotes;
 
 
     musicPlaying = false;
@@ -35,6 +35,7 @@ function init(){
 function imageIsLoaded(e) {
     $('#myImg').attr('src', e.target.result);
     ctx.clearRect(0,0,c.width,c.height);
+
     drawImage();
 }
 
@@ -44,12 +45,48 @@ function playMusic(){
     console.log("MusicPlayingStart");
 }
 
-function searchForRectangle(){
-    console.log("searchForRectangle");
+function searchForNotes(){
+    // array of all pixels in image, 4 indexes for each pixel. R,G,B,A
+    var imageData = ctx.getImageData(2,2,c.width-4,c.height-4);
+    for(var i = 0; i < (imageData.height*imageData.width*4); i++){
+        switch(i % 4){
+            case 0:{
+                if(imageData.data[i] > 75 && imageData.data[i+1] < 75 && imageData.data[i+1] < 75){
+                    //debugger;
+                    imageData.data[i] = 0;
+                    imageData.data[i+1] = 255;
+                    imageData.data[i+2] = 0;
+                    i += 3;
+                }
+                //imageData.data[i] = processValue(imageData.data[i], "red"); 
+            } break; // red
+            case 1:{
+                //imageData.data[i] = processValue(imageData.data[i], "green"); 
+            } break; // green
+            case 2:{
+                //imageData.data[i] = processValue(imageData.data[i], "blue");
+            } break; // blue
+            case 3:{
+
+            } break; // alpha
+        }
+    }
+    console.log("done");
+    ctx.putImageData(imageData,2,2);
+    // find rectangle
+    // find notes
+    console.log("searchForNotes");
 }
 
-function searchForNotes(){
-    console.log("searchForNotes");
+function processValue(value, color){
+    if(value > 175){
+        if(color == "green"){
+            value = 255;
+        } else {
+            value = 0;
+        }
+    }
+    return value;
 }
 
 function main(){
