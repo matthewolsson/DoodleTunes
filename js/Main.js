75297428686a3d29ad4da4206028ad6c84715d09
matthewolsson <molsson2@gmail.com>
2015-03-04 06:40:26 -0500

    Detecting blobs framework
    Added in code to detect flagged pixels surrounded by other flagged
    pixels, sensitivity is adjustable and still needs code to isolate,
    calculate, and purge, blobs once they are recognized.

--- js/Main.js
+++ js/Main.js
@@ -1,6 +1,8 @@
+// http://stackoverflow.com/questions/13385335/setting-uint8clampedarray-to-imagedata-is-very-slow-in-firefox
+
 "use strict"
 
-var c,ctx,img,playMusicBtn,findNotesBtn,musicPlaying,fps,count,difference,sensitivity,topLeftOfTune,bottomRightOfTune,stick;
+var c,ctx,img,playMusicBtn,findNotesBtn,musicPlaying,fps,count,difference,greySensitivity,topLeftOfTune,bottomRightOfTune,stick,noteSensitivity,foundPixels;
 
 window.onload = init;
 
@@ -29,9 +31,11 @@ function init(){
     fps = 60;
     count = 0;
     difference = 0;
-    sensitivity = 180; // higher is less sensitive
+    greySensitivity = 180; // higher is less sensitive
+    noteSensitivity = 4; // lower is less sensitive (must be an increment of 4)
     topLeftOfTune = {x:999,y:999};
     bottomRightOfTune = {x:0,y:0};
+    foundPixels = [];
 
     main();
 }
@@ -55,38 +59,28 @@ function playMusic(){
 function searchForNotes(){
     // array of all pixels in image, 4 indexes for each pixel. R,G,B,A - for loop indexes by pixel not value
     var imageData = ctx.getImageData(2,2,c.width-4,c.height-4);
-    for(var i = 0; i < (imageData.height*imageData.width*4); i+=4){
-        if(imageData.data[i] > sensitivity && imageData.data[i+1] > sensitivity && imageData.data[i+2] > sensitivity){ // if it's too close to white
-            // leave it alone
-        }
-        else { // make that shit green
 
-            difference = Math.abs(imageData.data[i] - imageData.data[i+1]);
-            difference += Math.abs(imageData.data[i] - imageData.data[i+2]);
-            difference += Math.abs(imageData.data[i+1] - imageData.data[i+2]);
-            if(difference < sensitivity/2.5){ // hopefully catches grays
-                // leave them alone
-            }
-            else {
-                // sets the top left and bottom right of the picture
-                if((i%(imageData.width*4))/4 < topLeftOfTune.x){
-                    topLeftOfTune.x = (i%(imageData.width*4))/4;
+    for(var i = 0; i < imageData.data.length; i+=4){
+        flagPixels(imageData,i); // colors all pixels that aren't white or black, green.
+    }
+
+    for(var i = 0; i < foundPixels.length; i++){
+        if(testPixel(imageData,foundPixels[i])){
+            detectCorners(imageData,foundPixels[i]); // sets the top left and bottom right of the picture
+        }
+        // looks for pixels that meet note qualifications (tests below, above, right, and left) NEEDS OPTIMIZING
+        if(testPixel(imageData,(foundPixels[i]+noteSensitivity))){ // checks to the right
+            if(testPixel(imageData,(foundPixels[i]-noteSensitivity))){ // checks to the left
+                if(testPixel(imageData,(foundPixels[i]+(imageData.width*noteSensitivity)))){ // checks below
+                    if(testPixel(imageData,(foundPixels[i]-(imageData.width*noteSensitivity)))){ // checks above
+                        processNotePosition(imageData,foundPixels[i]) // Not implemented   
+                    }
                 }
-                if((i/(imageData.width*4)) < topLeftOfTune.y){
-                    topLeftOfTune.y = (i/(imageData.width*4)); 
-                }
-                if((i%(imageData.width*4))/4 > bottomRightOfTune.x){
-                    bottomRightOfTune.x = (i%(imageData.width*4))/4; 
-                }
-                if((i/(imageData.width*4)) > bottomRightOfTune.y){
-                    bottomRightOfTune.y = (i/(imageData.width*4)); 
-                }
-                imageData.data[i] = 0;
-                imageData.data[i+1] = 255; // make green
-                imageData.data[i+2] = 0;
             }
         }
     }
+
+    // for debugging purposes, eventually combine find notes into play music
     console.log("done");
 
     ctx.putImageData(imageData,2,2);
@@ -110,6 +104,53 @@ function searchForNotes(){
 
     // find notes
     console.log("searchForNotes");
+}
+
+function testPixel(imageData,index){
+    if(imageData.data[index] === 0 && imageData.data[index+1] === 255 && imageData.data[index+2] === 0){ return true; }
+    else { return false; }
+}
+
+function flagPixels(imageData,index){
+    if(imageData.data[index] > greySensitivity && imageData.data[index+1] > greySensitivity && imageData.data[index+2] > greySensitivity){ // if it's too close to white
+        return false;
+    }
+    else { // pixel is not white
+
+    // calculates how grey it is
+        difference = Math.abs(imageData.data[index] - imageData.data[index+1]);
+        difference += Math.abs(imageData.data[index] - imageData.data[index+2]);
+        difference += Math.abs(imageData.data[index+1] - imageData.data[index+2]);
+        if(difference < greySensitivity/2.5){ // catches greys
+            return false;
+        }
+        else {
+            imageData.data[index] = 0;
+            imageData.data[index+1] = 255; // make green
+            imageData.data[index+2] = 0;
+            foundPixels[foundPixels.length] = index;
+        }
+    }
+}
+
+function detectCorners(imageData,index){
+    if((index%(imageData.width*4))/4 < topLeftOfTune.x){
+        topLeftOfTune.x = (index%(imageData.width*4))/4;
+    }
+    if((index/(imageData.width*4)) < topLeftOfTune.y){
+        topLeftOfTune.y = (index/(imageData.width*4)); 
+    }
+    if((index%(imageData.width*4))/4 > bottomRightOfTune.x){
+        bottomRightOfTune.x = (index%(imageData.width*4))/4; 
+    }
+    if((index/(imageData.width*4)) > bottomRightOfTune.y){
+        bottomRightOfTune.y = (index/(imageData.width*4)); 
+    }
+}
+
+// When a pixel is detected that has a pixel above, left, right, and below it that are also detected then it's put here for processing, In this function the program jumps up in horizontal line an arbitrary width to find the top of the note, then do the same thing down to find the bottom of the note, then left along a line the height of the note to find the left, and right to do the same. Then assign the center of the note to be the center of the created rectangle, and white the whole note out so it doesn't get caught again when the program continues to run. 
+function processNotePosition(imageData,index){
+    debugger;
 }
 
 function main(){
