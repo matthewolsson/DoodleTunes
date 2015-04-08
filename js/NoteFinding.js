bbada5fdee1788635972e6b12b41fa08d6ea24f7
matthew <molsson2@gmail.com>
2015-04-08 19:36:57 -0400

    fixed scales
    need compatibility with platforms

--- js/NoteFinding.js
+++ js/NoteFinding.js
@@ -94,20 +94,20 @@ function removeDuplicates(){
 function assignNotes(){
     var numberOfNotes = 13;
     for(var i = 0; i < noteArray.length; i++){
-        var adjustedY = noteArray[i].center.y-boundingBox.topLeft.y;
-        var percentageY = adjustedY/boundingBox.height*100;
+        var adjustedY = noteArray[i].center.y-(boundingBox.topLeft.y+borderSensitivity);
+        var percentageY = adjustedY/(boundingBox.height-borderSensitivity)*100;
         if(percentageY > (100/numberOfNotes*0) && percentageY <= (100/numberOfNotes*1)){noteArray[i].assignedNote = 'E';noteArray[i].assignedOctave = '5';continue; }
         if(percentageY > (100/numberOfNotes*1) && percentageY <= (100/numberOfNotes*2)){noteArray[i].assignedNote = 'D';noteArray[i].assignedOctave = '5';continue; }
         if(percentageY > (100/numberOfNotes*2) && percentageY <= (100/numberOfNotes*3)){noteArray[i].assignedNote = 'C';noteArray[i].assignedOctave = '5';continue; }
-        if(percentageY > (100/numberOfNotes*3) && percentageY <= (100/numberOfNotes*4)){noteArray[i].assignedNote = 'B';noteArray[i].assignedOctave = '5';continue; }
-        if(percentageY > (100/numberOfNotes*4) && percentageY <= (100/numberOfNotes*5)){noteArray[i].assignedNote = 'A';noteArray[i].assignedOctave = '5';continue; }
+        if(percentageY > (100/numberOfNotes*3) && percentageY <= (100/numberOfNotes*4)){noteArray[i].assignedNote = 'B';noteArray[i].assignedOctave = '4';continue; }
+        if(percentageY > (100/numberOfNotes*4) && percentageY <= (100/numberOfNotes*5)){noteArray[i].assignedNote = 'A';noteArray[i].assignedOctave = '4';continue; }
         if(percentageY > (100/numberOfNotes*5) && percentageY <= (100/numberOfNotes*6)){noteArray[i].assignedNote = 'G';noteArray[i].assignedOctave = '4';continue; }
         if(percentageY > (100/numberOfNotes*6) && percentageY <= (100/numberOfNotes*7)){noteArray[i].assignedNote = 'F';noteArray[i].assignedOctave = '4';continue; }
         if(percentageY > (100/numberOfNotes*7) && percentageY <= (100/numberOfNotes*8)){noteArray[i].assignedNote = 'E';noteArray[i].assignedOctave = '4';continue; }
         if(percentageY > (100/numberOfNotes*8) && percentageY <= (100/numberOfNotes*9)){noteArray[i].assignedNote = 'D';noteArray[i].assignedOctave = '4';continue; }
         if(percentageY > (100/numberOfNotes*9) && percentageY <= (100/numberOfNotes*10)){noteArray[i].assignedNote = 'C';noteArray[i].assignedOctave = '4';continue; }
-        if(percentageY > (100/numberOfNotes*10) && percentageY <= (100/numberOfNotes*11)){noteArray[i].assignedNote = 'B';noteArray[i].assignedOctave = '4';continue; }
-        if(percentageY > (100/numberOfNotes*11) && percentageY <= (100/numberOfNotes*12)){noteArray[i].assignedNote = 'A';noteArray[i].assignedOctave = '4';continue; }
+        if(percentageY > (100/numberOfNotes*10) && percentageY <= (100/numberOfNotes*11)){noteArray[i].assignedNote = 'B';noteArray[i].assignedOctave = '3';continue; }
+        if(percentageY > (100/numberOfNotes*11) && percentageY <= (100/numberOfNotes*12)){noteArray[i].assignedNote = 'A';noteArray[i].assignedOctave = '3';continue; }
         if(percentageY > (100/numberOfNotes*12) && percentageY <= (100/numberOfNotes*13)){noteArray[i].assignedNote = 'G';noteArray[i].assignedOctave = '3';continue; }
     }
     for(var i = 0; i < noteArray.length; i++){
@@ -119,6 +119,7 @@ function assignNotes(){
 function doSetNoteTimers(note,octave,timing,piano){
     setTimeout(function(){ 
         piano.play(note, octave, .5);
+        console.log(note + "" + octave);
     }, (4000/100)*timing);
 }
 
